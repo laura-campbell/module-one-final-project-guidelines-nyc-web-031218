82e77e202695d918833ac680239f6528a01fd9de
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-27 15:31:31 -0400

    API stuff

---
+++ CLI.rb
@@ -0,0 +1,16 @@
+
+
+
+def welcome
+  puts "Welcome to the Instagram Sentiment Assessor!"
+end
+
+def get_username
+  puts "Please enter your first and last name:"
+  gets.chomp
+end
+
+def get_profile
+  puts "Enter the username of a public profile you'd like to analyze the sentiment of:"
+  gets.chomp
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,26 @@
-require_relative 'config/environment'
+# require_relative 'config/environment.rb'
+# require_relative 'config/initializers.rb'
+require_relative "../CLI.rb"
+require_relative "../API_communicator.rb"
 
+
+def run
+  welcome
+  name = get_username
+  profile = get_profile
+  captions = get_profiles_from_input(profile)
+  binding.pry
+    if captions == nil
+      puts "This user is private. Sorry!"
+    else
+      sentiment = get_sentiment(captions)
+        if sentiment == nil
+          puts "This user doesn't like captions. Sorry!"
+        else
+          puts sentiment
+        end
+    end
+  end
+
+
+run
