f90ff50ecdbbc828aa16677a27934df91318056c
p-wong <patriciawong08@gmail.com>
2018-03-28 13:13:41 -0400

    Updated models again to add functionality

--- app/models/sentiment.rb
+++ app/models/sentiment.rb
@@ -2,6 +2,7 @@ class Sentiment < ActiveRecord::Base
   has_many :profiles
   has_many :users, through: :profiles
 
+  #Analysis: Option 6 - See the breakdown by positive/negative/netural of everyone who has been creeped.
   def self.count_of_all
     r = self.all.map { |x| x[:sentiment] }
     r.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
