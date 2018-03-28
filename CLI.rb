79e201d65fa9e85987b871cb90aa3dcab4883caf
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-28 13:13:48 -0400

    updated CLI and run file

--- CLI.rb
+++ CLI.rb
@@ -6,23 +6,35 @@ def welcome
 end
 
 def get_username
-  print "Please enter your first and last name: "
+  print "
+  Please enter your first name: "
   response = gets.chomp
   User.create(username:response)
 end
 
 def get_profile
-  print "Enter the username of a public profile you'd like to analyze the sentiment of: "
+  print "
+  Enter the username of a public profile you'd like to analyze the sentiment of: "
   gets.chomp
 end
 
 
-def help
-  puts "Options to do:
-    1. Enter another username of a public profile for sentiment analyzing
-    2. See all the profiles you've looked at
-    3. See the breakdown of sentiments from the profiles you've looked at
+def helper
+  puts "
+  What do you want to do next?
+  CONTINUE:
+      1. Enter another username of a public profile for sentiment analyzing
+  ANALYZE YOUR RESULTS:
+      2. See the results of the profiles you've looked at
+      3. See the breakdown of sentiments from the profiles you've looked at
+  ANALYZE ALL RESULTS:
+      4. See the number of profiles all users have looked at
+      5. See the results of all the profiles looked at by all users
+      6. See the breakdown of sentiments of all the profiles
+  QUIT:
+      7. Clear history
+      8. Exit the application
   "
-  print "What would you like to do? (input a number from options above) "
+  print "Please enter the number of your choice from the options above: "
   gets.chomp
 end
--- bin/run.rb
+++ bin/run.rb
@@ -7,26 +7,56 @@ require_relative "../API_communicator.rb"
 # require_relative "../app/models/user.rb"
 
 
+
 def run
   welcome
   name = get_username
+  run2(name)
+  option = helper
+    while option != '8'
+        if option == '1'
+          run2(name)
+        elsif option == '2'
+          name.profiles.each do |profile|
+            puts "#{profile.name} + #{profile.sentiment}"
+          end
+        elsif option == '3'
+          sentiment_counts
+        elsif option == '4'
+          User.num_of_profiles
+        elsif option == '5'
+          Profile.results
+        elsif option == '6'
+          Sentiment.count_of_all
+        elsif option == '7'
+          User.profiles.delete_all
+        else
+          puts "That's not a valid command. Please try again."
+        end
+      option = helper
+  end
+  puts "Session ended."
+end
+
+def run2(name)
   profile = get_profile
   profile_instance = name.add_profile(profile)
   captions = get_profiles_from_input(profile)
   if captions == nil
-    puts "This user is private. Sorry!"
+    puts "
+    This user is private. Sorry!"
   else
     sentiment = get_sentiment(captions).to_s
     if sentiment == nil
-      puts "This user doesn't like captions. Sorry!"
+      puts "
+      This user doesn't like captions. Sorry!"
     else
       profile_instance.sentiment_id(sentiment)
       profile_instance.save
     end
-    puts "#{profile}'s sentiment is: #{sentiment.capitalize}"
+    puts "
+    #{profile}'s sentiment is: #{sentiment.capitalize}"
   end
-  option = help
 end
-
 
 run
